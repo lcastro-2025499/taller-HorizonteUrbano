7698d1242a7079c484dd0f40525cb2754dd58e0f
Harold Panto <hpanto-2022332@kinal.edu.gt>
2026-09-16 11:56:38 -0600

    refactor: se eliminaron paquetes no pertenecientes a la rama

--- HorizonteUrbanoIn4am/src/org/horizonteurbano/system/config/Environment.java
+++
@@ -1,9 +0,0 @@
-package org.horizonteurbano.system.config;
-
-public class Environment {
-
-    protected static final String LOCATION_SERVICE = "localhost:3306";
-    protected static final String DATA_BASE = "ddl_auditoria_usuario_producto_IN4AM";
-    protected static final String USER = "USER";
-    protected static final String PASSWORD = "PASSWORD";
-}
